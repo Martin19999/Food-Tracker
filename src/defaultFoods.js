814d5dc0d0ba785495a49c2299d74cf217189797
Jianhao Feng <119100251@umail.ucc.ie>
2025-08-30 12:48:19 +0100

    add

--- src/defaultFoods.js
+++ src/defaultFoods.js
@@ -1,11 +1,27 @@
 const rawFoods = [
-  ["Milk", 106, 23.5, 100, "g"],
+  // Breakfast
+  ["Milk", 64, 3.4, 100, "ml"],
+  ["Egg", 81, 7.8, 1, ""],
+  ["Tesco penaut butter", 6.33, 0.245, 1, "g"],
 
+  // Carbs
+  ["Bagel raisin & cinnamon", 233, 8.2, 1, ""],
+  ["Oats", 37, 1.1, 10, "g"],
+  ["Penne rigate", 350, 12.5, 100, "g"],
 
-
-
-  // Lidl Meat
+  // ------------Common Seasonings-------------
+  ["Olive oil", 8, 0, 1, "ml"],
+  ["Soy sauce", 1.2, 0.092, 1, "ml"],
+  ["Seasame oil", 8.28, 0, 1, "ml"],
+  ["Rice vinegar", 0.15, 0, 1, "ml"],
+  ["Balsamic vinegar dressing", 3.49, 0.03, 1, "ml"],
+  ["Unsalted butter", 7.57, 0.005, 1, "g"],
+  ["Green Pesto", 4.64, 0.045, 1, "g"],
+  ["Honey", 3.29, 0, 1, "g"],
   
+
+
+  // ----------------Lidl Meat------------------
   //Chicken
   ["Chicken breast fillet", 106, 23.5, 100, "g"],
   ["Chicken drumsticks", 131, 18.7, 100, "g"],
@@ -18,7 +34,8 @@ const rawFoods = [
   //Seafood
   ["4 skin on salmon darnes", 204, 19.1, 100, "g"],
 
-  
+  // -------------------Veg----------------------
+
 ];
 
 export const defaultFoods = rawFoods.map(([name, calories, protein, per, perWhat]) => ({
